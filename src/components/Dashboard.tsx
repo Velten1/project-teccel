85ed31e551f70bb99f9dd49d9c32fe208ecc7cfe
velten1 <caioryan.ca@gmail.com>
2025-10-22 23:58:18 -0300

    feat: add dashboard skeleton for futures upgrade

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function Dashboard() {
+    const [name, setName] = useState("")
+    const [tel, setTel] = useState("")
+    const [email, setEmail] = useState("")
+    const [doubt, setDoubt] = useState("")
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Dashboard
