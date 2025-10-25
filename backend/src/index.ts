1a63f32423e11183419302b53687981d802a0b22
velten1 <caioryan.ca@gmail.com>
2025-10-25 19:06:27 -0300

    chore: create index for run server - created skeletons for future method MVC

---
+++ backend/src/index.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5001",
+    credentials: true,
+    methods: ["GET", "POST", "OPTIONS"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
+app.use(express.json())
+
+const PORT = process.env.PORT || 5001
+
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
